605ad14544a65b0de77a1ef3d4885eea60ed8af0
Arham Shakeel <l252597@lhr.nu.edu.pk>
2026-07-03 14:31:48 +0000

    datatypes done

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+console.log("arham")
+const VAR1 = 1233;
+const VAR2 = "hello world";
+const VAR3 = "hello"; 
+let VAR4 = 123.45;
+let VAR5 = "helloworld1";
+VAR6 = 1213;
+
+console.log(VAR1);
+console.table({VAR1, VAR2, VAR3, VAR4, VAR5, VAR6});
