53844febd053d35526de21a23ea26e258bc0d93a
Arham Shakeel <l252597@lhr.nu.edu.pk>
2026-07-25 09:52:17 +0000

    projects

---
+++ projects/project_02.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit',function(e){
+    e.preventDefault(); //prevents connecting to server
+const height = parseInt(document.querySelector('#height').value);
+const weight = parseInt(document.querySelector('#weight').value);
+const result = document.querySelector('#results');
+if(height === '' || height < 0 || isNaN(height))
+{
+  result.innerHTML = `please give a valid height ${height}`;
+}
+else if(weight === '' || weight < 0 || isNaN(weight))
+{
+  result.innerHTML = `please give a valid height ${weight}`;
+}
+else
+{const bmi =  (weight / ((height * height) / 10000)).toFixed(2);
+
+  if(bmi<18.6)
+  {
+    result.innerHTML = `<span> ${bmi} -> UnderWeight </span>`;
+  }
+  else if(bmi>24.9)
+  {
+    result.innerHTML = `<span>${bmi} -> OverWeight </span>`;
+  }
+  else{
+    result.innerHTML = `<span>${bmi} -> Normal </span>`;
+  }
+}
+
+})
+//input has a VALUE method
