67530ce4d0dfe1e21fbf8c423fd6f7bbce2696f9
Arham Shakeel <arhamshakeel2015@gmail.com>
2026-07-28 14:48:42 +0500

    static

---
+++ classes_and_objects/staticprop.js
@@ -0,0 +1,29 @@
+// static -> A property or method that belongs to the class
+// itself instead of its objects.
+
+  class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const hitesh = new User("hitesh")
+// console.log(hitesh.createId())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId());
